e033afdd23b1519d7575dbc10c33688a6249c39d
Christoffer Jerrebo <christoffer.jerrebo@jayway.com>
2020-10-30 16:26:04 +0100

    .NET example

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Com.Jayway.Kday.Grpc;
+using Grpc.Net.Client;
+using static Com.Jayway.Kday.Grpc.Puzzle;
+
+namespace GrpcPuzzleClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var channel = GrpcChannel.ForAddress("https://grpc-puzzle-pvufxpciqa-lz.a.run.app");
+            var client = new PuzzleClient(channel);
+
+            var clueOne = client.StartHere(new YourName { YourName_ = "foo" });
+            Console.WriteLine("1: " + clueOne.Clue);
+
+            var clueTwo = client.endpointOne(clueOne);
+            Console.WriteLine("2: " + clueTwo.Clue);
+
+            var clueThree = client.endpointTwo(clueTwo);
+            Console.WriteLine("3: " + clueThree.Clue);
+
+            var clueFour = client.endpointThree(clueThree);
+            Console.WriteLine("4: " + clueFour.Clue);
+
+            var final = client.SolvePuzzle(new Key { Key_ = "developyourcompetenceyoumust" });
+            Console.WriteLine("Final: " + final.FinalSecret_);
+        }
+    }
+}
